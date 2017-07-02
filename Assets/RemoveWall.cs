f4181278f84370a3562a2c5e2fca6f0043e3b66c
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-02 06:13:33 -0300

    Removed unnecessary thing
    by [Dara Keon (IK)] in [2016-09-13 16:50:04 (-03:00)]

--- Assets/RemoveWall.cs
+++ Assets/RemoveWall.cs
@@ -14,16 +14,4 @@ public class RemoveWall : MonoBehaviour
 		collision.gameObject.renderer.enabled = true;
 		Debug.Log(collision.gameObject.name);
 	}
-
-	void OnTriggerEnter(Collider collider)
-	{
-		collider.gameObject.renderer.enabled = false;
-		Debug.Log(collider.gameObject.name);
-	}
-
-	void OnTriggerExit(Collider collider)
-	{
-		collider.gameObject.renderer.enabled = true;
-		Debug.Log(collider.gameObject.name);
-	}
 }
