0073dc3b00016851b744cccaf787ffe261b2ad8f
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-02 06:13:21 -0300

    Removing walls when walk
    by [Dara Keon (IK)] in [2016-09-13 16:46:30 (-03:00)]

---
+++ Assets/RemoveWall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveWall : MonoBehaviour
+{
+	void OnCollisionEnter(Collision collision)
+	{
+		collision.gameObject.renderer.enabled = false;
+		Debug.Log(collision.gameObject.name);
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		collision.gameObject.renderer.enabled = true;
+		Debug.Log(collision.gameObject.name);
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		collider.gameObject.renderer.enabled = false;
+		Debug.Log(collider.gameObject.name);
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		collider.gameObject.renderer.enabled = true;
+		Debug.Log(collider.gameObject.name);
+	}
+}
