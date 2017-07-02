ff5297e3d75ff7db1f699d4e7f6c8fceb0f15b88
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-02 06:12:55 -0300

    Project start  Walls  Camera/UX
    by [darakeon] in [2016-09-13 16:20:59 (-03:00)]

---
+++ Assets/Control.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour
+{
+	public float positionMoveUnit;
+	public float rotationMoveUnit;
+	
+	private Vector3 initialPosition;
+	private Vector3 initialRotation;
+	
+	
+	void Start()
+	{
+		initialPosition = this.transform.position;
+		initialRotation = this.transform.rotation.eulerAngles;
+	}
+	
+	
+	void Update ()
+	{
+		changePosition();
+		changeRotation();
+	}
+
+	private void changePosition()
+	{
+		if (!Input.anyKey)
+			return;
+
+		var diff = Vector3.zero;
+		var unit = positionMoveUnit;
+
+		if (Input.GetKey(KeyCode.PageDown))
+			diff.y -= unit;
+
+		if (Input.GetKey(KeyCode.PageUp))
+			diff.y += unit;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+			diff.x -= unit;
+
+		if (Input.GetKey(KeyCode.RightArrow))
+			diff.x += unit;
+
+		if (Input.GetKey(KeyCode.DownArrow))
+			diff.z -= unit;
+
+		if (Input.GetKey(KeyCode.UpArrow))
+			diff.z += unit;
+
+		if (Input.GetKey(KeyCode.Q))
+			diff = initialPosition - this.transform.position;
+
+		this.transform.Translate(diff.x, diff.y, diff.z);
+	}
+
+	private void changeRotation()
+	{
+		if (!Input.anyKey)
+			return;
+
+		var diff = Vector3.zero;
+		var unit = rotationMoveUnit;
+
+		if (Input.GetKey(KeyCode.S))
+			diff.x -= unit;
+
+		if (Input.GetKey(KeyCode.W))
+			diff.x += unit;
+
+		if (Input.GetKey(KeyCode.A))
+			diff.y -= unit;
+
+		if (Input.GetKey(KeyCode.D))
+			diff.y += unit;
+
+		if (Input.GetKey(KeyCode.Q))
+			diff = initialRotation - this.transform.rotation.eulerAngles;
+
+		this.transform.Rotate(diff.x, diff.y, diff.z);
+	}
+
+
+}
